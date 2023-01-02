18c7571b0ca929559dc0bfa244774d676db44bc8
Dmytro Lopushanskyy <dmytrolopushanskyy@gmail.com>
2023-01-02 11:16:25 -0500

    Update data_access.py

--- secondary-node/data_access.py
+++ secondary-node/data_access.py
@@ -1,6 +1,7 @@
 """
 This module is responsible for getting the data for the facade service.
 """
+from application.custom_logger import logger
 
 
 class DataAccess:
@@ -9,7 +10,10 @@ class DataAccess:
 
     def save_data(self, msg, msg_id) -> None:
         """ Save data to local memory """
-        self.__data.append((msg_id, msg))
+        if (msg_id, msg) not in self.__data:
+            self.__data.append((msg_id, msg))
+            self.__data.sort(key=lambda item: item[0])  # Sort it
+            logger.info(f"Data: {[item[1] for item in self.__data]}")
 
     def get_data(self):
         gap_idx = self.find_first_gap_idx()
@@ -22,6 +26,12 @@ class DataAccess:
         Assumes data indexing starts at 0 index.
         """
         last_id = -1
+
+        # Allow data to start **not** with ID=0. Will be useful if the secondary node is rebooted
+        # and the in-memory list is lost. Then it will be accumulating data from some later point of time.
+        if len(self.__data) > 0:
+            last_id = self.__data[0][0] - 1
+
         for item in self.__data:
             msg_id = item[0]
             if msg_id - last_id != 1:
